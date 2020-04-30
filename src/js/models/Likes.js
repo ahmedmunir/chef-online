d89236a8c8576c56c3b4da80e9c0ef57ff95a7db
ahmedmunir <mryagamie@outlook.com>
2020-04-30 16:11:39 +0200

    Add likes directory to project

---
+++ src/js/models/Likes.js
@@ -0,0 +1,8 @@
+export default class Likes {
+    constructor(ID, label, source, image) {
+        this.ID = ID;
+        this.label = label;
+        this.source = source;
+        this.image = image;
+    }
+}
